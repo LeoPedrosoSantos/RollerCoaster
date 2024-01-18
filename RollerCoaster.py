175810f9500b5671e36739d994cc5ba2fef9740b
Leonardo Pedroso dos Santos <leonardopedroso27@yahoo.com.br>
2024-01-18 19:31:23 -0300

    RollerCoaster ticket

---
+++ RollerCoaster.py
@@ -0,0 +1,10 @@
+print("***Welcome to the RollerCoaster***\n")
+
+height = int(input("How tall is you in cm?\n"))
+
+if height >= 150:
+    print("You can go, have fun\n")
+else:
+    print("Sorry, you can't go to the rollercoaster, come back when you are taller\n")
+
+print(input("Click ENTER to close"))
